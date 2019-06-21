e6e223330c79bb9c39def974717b6e756b205242
Philipp Rouast <prouast@gmail.com>
2019-06-21 13:23:02 +1000

    Add more content

--- js/script.js
+++ js/script.js
@@ -3,34 +3,47 @@ $(document).ready(function() {
   $("#topMenu").click(function() {
     $('html, body').animate({
         scrollTop: 0
-    }, 250);
+    }, 200);
   });
   $("#projectsMenu").click(function() {
     $('html, body').animate({
         scrollTop: $("#projects").offset().top - 100
-    }, 250);
+    }, 200);
   });
   $("#publicationsMenu").click(function() {
     $('html, body').animate({
         scrollTop: $("#publications").offset().top - 100
-    }, 250);
+    }, 200);
   });
   $("#timelineMenu").click(function() {
     $('html, body').animate({
         scrollTop: $("#timeline").offset().top - 100
-    }, 250);
+    }, 200);
   });
   $("#projectsButton").click(function(evt){
     evt.preventDefault();
     if ($("#hidableProjects").css("display") == "none") {
-      $("#hidableProjects").show(250);
+      $("#hidableProjects").show(0);
       $("#projectsButton").html("Show less")
       $("#projectsButton").removeClass("primary")
     }
     else {
-      $("#hidableProjects").hide(250);
+      $("#hidableProjects").hide(0);
       $("#projectsButton").html("Show more")
       $("#projectsButton").addClass("primary")
+    }
+  });
+  $("#publicationsButton").click(function(evt){
+    evt.preventDefault();
+    if ($("#hidablePublications").css("display") == "none") {
+      $("#hidablePublications").show(0);
+      $("#publicationsButton").html("Show less")
+      $("#publicationsButton").removeClass("primary")
+    }
+    else {
+      $("#hidablePublications").hide(0);
+      $("#publicationsButton").html("Show more")
+      $("#publicationsButton").addClass("primary")
     }
   });
   $("#buttonTAC").click(function(evt){
@@ -61,6 +74,36 @@ $(document).ready(function() {
       $("#moreECIS").toggle();
       $("#buttonECIS").html("Read more")
       $("#buttonECIS").addClass("primary")
+    }
+  });
+  $("#buttonFCS").click(function(evt){
+    evt.preventDefault();
+    if ($("#moreFCS").css("display") == "none") {
+      $("#dotsFCS").toggle();
+      $("#moreFCS").toggle();
+      $("#buttonFCS").html("Read less")
+      $("#buttonFCS").removeClass("primary")
+    }
+    else {
+      $("#dotsFCS").toggle();
+      $("#moreFCS").toggle();
+      $("#buttonFCS").html("Read more")
+      $("#buttonFCS").addClass("primary")
+    }
+  });
+  $("#buttonAITIC").click(function(evt){
+    evt.preventDefault();
+    if ($("#moreAITIC").css("display") == "none") {
+      $("#dotsAITIC").toggle();
+      $("#moreAITIC").toggle();
+      $("#buttonAITIC").html("Read less")
+      $("#buttonAITIC").removeClass("primary")
+    }
+    else {
+      $("#dotsAITIC").toggle();
+      $("#moreAITIC").toggle();
+      $("#buttonAITIC").html("Read more")
+      $("#buttonAITIC").addClass("primary")
     }
   });
 });
